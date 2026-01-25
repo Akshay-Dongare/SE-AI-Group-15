d4164021991fbdfacfd5fa58a90d0af85a0dddf1
Akshay Dongare <akshay@akshaydongare.com>
2026-01-25 17:59:31 -0500

    Add my_hc.py for random data analysis

---
+++ Homework 1/my_hc.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3 -B
+# ./rand.py $RANDOM ~/gits/moot/optimize/misc/auto93.csv
+
+import random, sys, xai
+xai.the.data=sys.argv[2]
+random.seed(int(sys.argv[1]))
+
+data = xai.Data(xai.csv(xai.the.data))
+
+def Y(r): return round(xai.disty(data,r),2)
+
+def report(what,rows):
+    a=sorted(rows[:],key=Y)
+    print(f":n {len(a):4} :lo {Y(a[0]):5.2f} :mid {Y(a[len(a)//2]):5.2f}", what)
+
+report("baseline",data.rows)
+report("sample", xai.shuffle(data.rows)[:30])
