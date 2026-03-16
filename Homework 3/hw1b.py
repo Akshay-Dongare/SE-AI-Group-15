51bfda2d1473e30ad3676220f5c285c3027ed590
Akshay Dongare <akshay@akshaydongare.com>
2026-03-16 18:23:15 -0400

    hw1b solution

--- Homework 3/hw1b.py
+++ Homework 3/hw1b.py
@@ -15,18 +15,19 @@ def weibull(n=20):
 sizes = []
 for trial in range(50):
   rxs = {i: weibull() for i in range(20)}
+  pooled = [x for xs in rxs.values() for x in xs]
+  sd     = statistics.stdev(pooled)
 
-  # baseline = pool all observations
-  pooled = []  # TODO: flatten all rxs values
-  sd     = 0   # TODO: statistics.stdev(pooled)
-
-  winners = top(rxs, eps=0.35 * sd)
+  eps = 0.35 * sd
+  winners = top(rxs, eps=eps)
   sizes.append(len(winners))
 
-  # TODO: check that all winner means are
-  #       within eps of each other
+  winner_means = [statistics.mean(rxs[w]) for w in winners]
+  if winner_means:
+    center = statistics.mean(winner_means)
+    assert all(abs(m - center) <= eps for m in winner_means)
 
 print(f"avg winners: {sum(sizes)/len(sizes):.1f}/20")
 print(f"min winners: {min(sizes)}")
 print(f"max winners: {max(sizes)}")
-# TODO: does larger eps -> more or fewer winners? why?
+print("larger eps -> more winners (harder to justify practical differences)")
